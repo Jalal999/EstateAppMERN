b3db9e0b2df6175d386fa448917e677ba7349b36
Jalal999 <jmammadli2021@ada.edu.az>
2024-06-25 19:23:01 +0200

    auth: implementation of authentication
     - user schema with prisma
     - added api endpoints for login and register
     - saving user info into db
     - reading user info from db

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,18 +1,73 @@
 import bcrypt from "bcrypt"
+import prisma from "../lib/prisma.js"
+import jwt from "jsonwebtoken"
 
 export const register = async (req, res) => {
     console.log(req.body)
     const { username, email, password } = req.body
-    // hash password
-    const hashedPasswrod = await bcrypt.hash(password, 10)
-    console.log(hashedPasswrod)
-    // create a new user and save to db
+    try {
+        const hashedPasswrod = await bcrypt.hash(password, 10)
+        console.log(hashedPasswrod)
+        // create a new user and save to db
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password: hashedPasswrod
+            }
+        })
+        res.status(201).json({message: "User created successfully..."})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: "Failed to create a user"})
+    }
 }
 
-export const login = (req, res) => {
-    
+export const login = async (req, res) => {
+    const { username, password } = req.body
+
+    try {
+        // CHECK IF USER EXISTS
+        const user = await prisma.user.findUnique({
+            where: { username }
+        })
+
+        if(!user) {
+            return res.status(401).json({message: "Invalid credentials..."})
+        }
+
+        // CHECK IF PASSWORD IS CORRECT
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+
+        if(!isPasswordValid) {
+            return res.status(401).json({message: "Invalid credentials..."})
+        }
+
+        // GENERATE COOKIE TOKEN AND SEND IT TO USER
+        const age = 1000 * 60 * 60 * 24 * 7
+
+        const token = jwt.sign(
+            {
+                id: user.id
+            },
+            process.env.JWT_SECRET_KEY, 
+            {
+                expiresIn: age
+            })
+
+        const { password:userPassword, ...userInfo } = user
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            // secure: true // usually for production as it is with https. locally when we run, we send to http://localhost; therefore, no need for that
+            maxAge: age
+        }).json(userInfo)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: "Failed to login!"})
+    }
 }
 
 export const logout = (req, res) => {
-    
+    res.clearCookie("token").status(200).json({ message: "Logout successfully!" })
 }
